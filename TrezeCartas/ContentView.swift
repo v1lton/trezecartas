6b4819aa395b57e5c7483054c51bfee53b00ae29
Wilton Ramos <wrs@cin.ufpe.br>
2021-02-05 14:20:40 -0300

    first commit

---
+++ TrezeCartas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrezeCartas
+//
+//  Created by Wilton Ramos on 05/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
