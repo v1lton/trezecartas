bc581400734103f584e4aa2c7fae4dafdd866f44
Wilton Ramos <wrs@cin.ufpe.br>
2021-02-11 17:28:57 -0300

    Adiciona FinalGame.swift ao projeto

---
+++ TrezeCartas/FinalGame.swift
@@ -0,0 +1,20 @@
+//
+//  FinalGame.swift
+//  TrezeCartas
+//
+//  Created by Wilton Ramos on 11/02/21.
+//
+
+import SwiftUI
+
+struct FinalGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinalGame_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalGame()
+    }
+}
