3a0c0414b5c82f1b5ea765634d3ebff831815e9e
Wilton Ramos <wrs@cin.ufpe.br>
2021-02-11 23:17:12 -0300

    Cria State isCardShowingBack

--- TrezeCartas/ContentView2.swift
+++ TrezeCartas/ContentView2.swift
@@ -10,17 +10,16 @@ import SwiftUI
 
 struct ContentView2: View {
     
+    
     @State var health = 10
     @State var money = 10
     @State var drugs = 0
-    
     @State var leftOption: String = ""
     @State var rightOption: String = ""
-    
     @State var maxIntID: Int = 12
-    
     @State var cards = CardData().getShuffledCards()
-
+    @State var isCardShowingBack = false
+    
     /// Return the CardViews width for the given offset in the array
     /// - Parameters:
     ///   - geometry: The geometry proxy of the parent
@@ -63,62 +62,72 @@ struct ContentView2: View {
                                     // Remove that user from our array
                                     maxIntID -= 1 // reduz o id maximo
                                     self.cards.removeAll { $0.id == removedCard.id }
-                                }, health: $health, money: $money, drugs: $drugs, maxID: $maxIntID, leftOption: $leftOption, rightOption: $rightOption)
+                                }, health: $health, money: $money, drugs: $drugs, maxID: $maxIntID, leftOption: $leftOption, rightOption: $rightOption, isCardShowingBack: $isCardShowingBack)
                                 .animation(.spring())
                                 .frame(width: self.getCardWidth(geometry, id: cardss.id), height: 500)
                                 .offset(x: 0, y: self.getCardOffset(geometry, id: cardss.id))
-  
+                                
                             }
                         }
                     }
                     
                     Spacer().frame(height: 50)
-                    HStack {
-                        Button(action: {
-                            //self.teste = true
-                        }, label: {
-                            HStack {
-                                Spacer()
-                                Text(leftOption)
-                                    //.font(.custom("Raleway-Bold", size: 18))
-                                    .font(.system(size: 17)) // era 20
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.brancoColor)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(2)
-                                    .padding(10)
-                                Spacer()
+                    
+                    if !self.isCardShowingBack {
+                        HStack {
+                            Button(action: {
+                                //self.teste = true
+                            }, label: {
+                                HStack {
+                                    Spacer()
+                                    Text(leftOption)
+                                        //.font(.custom("Raleway-Bold", size: 18))
+                                        .font(.system(size: 17)) // era 20
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.brancoColor)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(2)
+                                        .padding(10)
+                                    Spacer()
+                                }
+                                
+                            })
+                            .frame(height: 65)
+                            .clipped()
+                            .background(Color.roxoClaroColor)
+                            .cornerRadius(10)
+                            if isCardShowingBack {
+                                Text("Aimds")
                             }
                             
-                        }).frame(height: 65)
-                        .clipped()
-                        .background(Color.roxoClaroColor)
-                        .cornerRadius(10)
-                        
-                        Spacer()
-                            .frame(width: 7)
-                        
-                        Button(action: {
-                            // acao do botao
-                        }, label: {
-                            HStack {
-                                Spacer()
-                                Text(rightOption)
-                                    .font(.system(size: 17)) // era 20
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.brancoColor)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(2)
-                                    .padding(10)
-                                Spacer()
-                            }
+                            Spacer()
+                                .frame(width: 7)
                             
-                        }).frame(height: 65)
-                        .clipped()
-                        .background(Color.roxoClaroColor)
-                        .cornerRadius(10)
+                            Button(action: {
+                                //action
+                            }, label: {
+                                HStack {
+                                    Spacer()
+                                    Text(rightOption)
+                                        .font(.system(size: 17)) // era 20
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.brancoColor)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(2)
+                                        .padding(10)
+                                    Spacer()
+                                }
+                                
+                            }).frame(height: 65)
+                            .clipped()
+                            .background(Color.roxoClaroColor)
+                            .cornerRadius(10)
+                        }
+                        .shadow(radius: 5)
+                    } else {
+                        Text("Bora dale!")
+                            .frame(height: 65)
                     }
-                    .shadow(radius: 5)
                     
                     Spacer()
                         .frame(height: 100)
