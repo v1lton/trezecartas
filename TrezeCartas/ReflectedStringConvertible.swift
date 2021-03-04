dfc6617ae58f66a4695b98cc07fa95889a665fb5
Samuel <samuelbsantosn@gmail.com>
2021-03-04 17:38:04 -0300

    Criando a decodificação do CSV em formato JSON para classes swift

---
+++ TrezeCartas/ReflectedStringConvertible.swift
@@ -0,0 +1,34 @@
+//
+//  ReflectedStringConvertible.swift
+//  TrezeCartas
+//
+//  Created by Samuel Brasileiro on 04/03/21.
+//
+
+import Foundation
+
+public protocol ReflectedStringConvertible : CustomStringConvertible { }
+
+extension ReflectedStringConvertible {
+  public var description: String {
+    let mirror = Mirror(reflecting: self)
+    
+    var str = "\(mirror.subjectType)("
+    var first = true
+    for (label, value) in mirror.children {
+      if let label = label {
+        if first {
+          first = false
+        } else {
+          str += ", "
+        }
+        str += label
+        str += ": "
+        str += "\(value)"
+      }
+    }
+    str += ")"
+    
+    return str
+  }
+}
